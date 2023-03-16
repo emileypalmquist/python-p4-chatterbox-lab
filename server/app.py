da58c98b0c8ce6a5cbabff449ee3567fbaf54d99
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2023-03-16 16:57:23 -0700

    review solution code

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, make_response, jsonify
+# request docs -> https://tedboy.github.io/flask/generated/generated/flask.Request.html
 from flask_cors import CORS
 from flask_migrate import Migrate
 
@@ -14,13 +15,59 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+@app.route('/messages', methods=['POST', 'GET'])
+def messages():
+    if request.method == 'GET':
+        messages = Message.query.order_by('created_at').all()
+        # dict_messages = []
+        # for message in messages:
+        #     dict_messages.append(message.to_dict())
+        dict_messages = [message.to_dict() for message in messages]
+
+        response = make_response(dict_messages, 200)
+        return response
+    elif request.method == 'POST':
+        data = request.get_json()
+
+        message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(message.to_dict(), 201)
+        return response
+
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(message.to_dict(), 200)
+        return response
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_dict = {
+            "message": "Message delete"
+        }
+
+        response = make_response(response_dict, 200)
+        return response
+    elif request.method == 'GET':
+        response = make_response(message.to_dict())
+        return response
+
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
